09fc34e26aa48f0faabdd5a13b6c5b42e1e82f39
Ashton Hotke <339741886@GoTVDSB.ca>
2024-09-24 08:57:59 -0400

    finished

--- Assignment 6 - Random Numbers/Program.cs
+++ Assignment 6 - Random Numbers/Program.cs
@@ -5,16 +5,19 @@
         static void Main(string[] args)
         {
             Random generator = new Random();
-            int guess, secretNumber, min, max;
+            int guess, secretNumber, min, max, num, die1, die2, sum;
 
             // Random numbers
-            Console.WriteLine("Please enter a minimum value: ");
-            Console.Read();
-            Console.WriteLine("Please enter a maximum value: ");
-            Console.Read();
+            Console.WriteLine("Please enter a minimum value, as a whole number: ");
+            Int32.TryParse(Console.ReadLine(), out min);
+            Console.WriteLine("Please enter a maximum value, as a whole number: ");
+            Int32.TryParse(Console.ReadLine(), out max);
 
+            num = generator.Next(min, max);
+            Console.WriteLine("The number I randomly picked from that range is " + num);
 
-
+            Console.WriteLine();
+            
 
 
             // Better guessing game
@@ -23,6 +26,7 @@
             Console.WriteLine("Let's play a game!");
             Console.WriteLine("I chose a number between 1 and 10, can you guess it?");
             Console.Write("Your guess: ");
+            Console.WriteLine();
             Int32.TryParse(Console.ReadLine(), out guess);
             if (guess == secretNumber)
                 Console.WriteLine("You guessed my secret number!");
@@ -35,6 +39,16 @@
             else if (guess < 1)
                 Console.WriteLine("I said from 1 to 10! That's way too low.");
             Console.WriteLine();
+
+            // Roll the dice
+
+            die1 = generator.Next(1, 6);
+            die2 = generator.Next(1, 6);
+            sum = die1 + die2;
+
+            Console.WriteLine("I rolled two dice. The first was a " + die1 + " and the second was a " + die2);
+            Console.WriteLine("Their sum is " + sum);
+            Console.ReadLine();
         }
     }
 }
